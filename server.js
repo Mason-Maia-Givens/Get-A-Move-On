cefe37cb95137aea755d1b5825fd7f0b369c43e4
Mason McCoy <mmasonmccoy@gmail.com>
2022-07-13 17:48:12 -0500

    Cleaning up

--- server.js
+++ server.js
@@ -1,39 +1,18 @@
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 // MVC-style routes
 const routes = require('./controllers');
 
-// Import helper functions
-// const helpers = require('./utils/helpers');
-
 // db connection with credentials
 const sequelize = require('./config/connection');
-
-// Sets up Session Store
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create(); // { helpers }
-
-// Instantiate session
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-
-// Pass session to application
-// app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
