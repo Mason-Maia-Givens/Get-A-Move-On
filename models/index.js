066ddd385cd088fcd9968b591421add4add5a5da
Maia Davis <maia@Maias-MacBook-Air.local>
2022-07-15 18:40:05 -0400

    associations for models created

--- models/index.js
+++ models/index.js
@@ -1,17 +1,54 @@
 const User = require('./User');
 const Move = require('./Move');
+const Client = require('./Client');
+const Mover = require('./Mover');
 
-User.hasMany(Move, {
-    foreignKey: 'user_id',
+Client.hasMany(Move, {
+    foreignKey: 'client_id',
     onDelete: 'CASCADE',
 });
 
-User.belongsTo(Move, {
-    foreignKey: 'user_id',
+Mover.hasMany(Move, {
+    foreignKey: 'mover_id',
+    onDelete: 'CASCADE',
 });
 
-Move.hasOne(User, {
-    as: 'client',
-    foreignKey: ''
+Client.belongsTo(Move, {
+    foreignKey: 'client_id',
+});
 
-}) 
+Mover.belongsTo(Move, {
+    foreignKey: 'mover_id',
+}); 
+
+Move.hasOne(Client, {
+    as: 'client',
+    foreignKey: 'client_id',
+    onDelete: 'CASCADE',
+});
+
+Move.hasOne(Mover, {
+    as: 'mover',
+    foreignKey: 'mover_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany{Client, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+}};
+
+Client.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany{Mover, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+}};
+
+Mover.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
